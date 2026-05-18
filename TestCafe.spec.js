5c844eedf94925a053ce9e1ef891a2bbb603b422
arielbinder <93533173+arielbinder@users.noreply.github.com>
2026-05-18 17:41:17 +0300

    Update TestCafe.spec.js

--- TestCafe.spec.js
+++ TestCafe.spec.js
@@ -38,44 +38,40 @@ test('TestCafe EX3', async ({ page }) => {
 });
 test('TestCafe EX5', async ({ page }) => {
     await page.goto('https://devexpress.github.io/testcafe/example');
-    await page.pause()
+    
     await page.getByTestId('name-input').type('Ariel'); 
-    await page.pause()
+    
        await page.getByRole('checkbox', { name: 'Support for testing on remote devices' }).check();
         await page.getByRole('checkbox', { name: 'Re-using existing JavaScript code for testing' }).check();
         await page.getByRole('checkbox', { name: 'Running tests in background and/or in parallel in multiple browsers' }).check();
         await page.getByRole('checkbox', { name: 'Easy embedding into a Continuous integration system' }).check();
         await page.getByRole('checkbox', { name: 'Advanced traffic and markup analysis' }).check();
-            await page.pause()
+           
    // אימות (אופציונלי) - בדיקה שכל התיבות אכן מסומנת
     await expect(page.getByRole('checkbox', { name: 'Support for testing on remote devices' })).toBeChecked();
     await expect(page.getByRole('checkbox', { name: 'Re-using existing JavaScript code for testing' })).toBeChecked();
     await expect(page.getByRole('checkbox', { name: 'Running tests in background and/or in parallel in multiple browsers' })).toBeChecked();
     await expect(page.getByRole('checkbox', { name: 'Easy embedding into a Continuous integration system' })).toBeChecked();
     await expect(page.getByRole('checkbox', { name: 'Advanced traffic and markup analysis' })).toBeChecked();
-            await page.pause()   
+              
     await page.getByRole('checkbox', { name: 'I have tried TestCafe' }).click();
-            await page.pause()
-    await page.getByRole('radio', { name: 'Windows' }).check();
-            await page.pause() 
+           
+    await page.getByRole('radio', { name: 'Windows' }).check(); 
     await expect(page.getByRole('radio', { name: 'Windows' })).toBeChecked(); 
-            await page.pause()
+          // בחירת אופיציה ובדיקת ערך מתץוך רשחמה נפתחת 
     await page.getByTestId('preferred-interface-select').selectOption('JavaScript API'); 
-            await page.pause()
     await expect(page.getByTestId('preferred-interface-select')).toHaveValue('JavaScript API'); 
-     await page.pause()
+     // הגדרת ערך קבוע וסימון תיבת טקסט
      const myText = 'This is my first line.\nThis is my second line.\nThis is my third line.';
-         await page.pause()
     await page.getByTestId('tried-testcafe-checkbox').check(); 
-        await page.pause()
-    await page.getByTestId('comments-area').fill(myText); 
-            await page.pause()
+       // הכנסת ערך קבוע של המשתמש ובדיקת הערך שהוגדר  
+    await page.getByTestId('comments-area').fill(myText);  
     await expect(page.getByTestId('comments-area')).toHaveValue(myText); 
-         await page.pause()
+         // לחיצה על כפתור 
         await page.getByRole('button', { name: 'Submit' }).click();
-            await page.pause()
+            // כניסה לעמוד "תודה" לאחר מילוי פרטים
             await page.goto('https://devexpress.github.io/testcafe/example/thank-you.html');
-            await page.pause()  
+          // בדיקה שהעמוד מכיל טקסט
            await expect(page.getByTestId('thank-you-header')).toContainText('Thank you, Ariel!');
 
 
